85b018058eb96cc61a87a806a006423a76a3e9a7
TheoVerhelst <tverhels@ulb.ac.be>
2016-01-29 19:16:58 +0100

    Move memset and memcpy

--- include/kernel/memcpy.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MEMCPY_HPP
-#define MEMCPY_HPP
-
-#include <kernel/_memtypes.hpp>
-#include <stddef.h>
-
-void *memcpy(void *, const void *, size_t);
-
-#endif// MEMCPY_HPP
--- include/string.hpp
+++ include/string.hpp
@@ -2,10 +2,16 @@
 #define STRING_HPP
 
 #include <stddef.h>
-#include <kernel/memset.hpp>
-#include <kernel/memcpy.hpp>
+#include <kernel/_memtypes.hpp>
 
 char *strrchr(const char *, int);
-size_t strlen(const char *); 
+size_t strlen(const char *);
+
+void *memsetb(void *, BYTE, size_t);
+void *memsetw(void *, WORD, size_t);
+void *memsetd(void *, DWORD, size_t);
+
+void *memcpy(void *, const void *, size_t);
+
 
 #endif// STRING_HPP
