ce24b95a371bd55fca71118224c4529791025e41
TheoVerhelst <tverhels@ulb.ac.be>
2016-02-19 13:32:47 +0100

    Improve ISR dispatcher
    The ISR dispatcher now send a message to the PIC if needed (if an IRQ is
    handled) and display more informations avout the error code.
    
    The error code is a structure that contains some boolean values about
    the exception, these values can now easily be retrieved.

--- include/kernel/interrupts/isrDispatcher.hpp
+++ include/kernel/interrupts/isrDispatcher.hpp
@@ -3,8 +3,18 @@
 
 #include <cstdint>
 
+struct ErrorCode
+{
+	uint8_t externalEvent:1;
+	uint8_t descriptorLocation:1;
+	uint8_t gdtLdt:1;
+	uint16_t segmentSelectorIndex:13;
+	uint16_t reserved;
+} __attribute__((packed));
+static_assert(sizeof(ErrorCode) == 4, "Error code structure must be 32-bit");
+
 extern "C" void isrDispatcher(uint32_t gs, uint32_t fs, uint32_t es,
-		uint32_t ds, uint32_t interruptNumber, uint32_t errorCode, uint32_t eip,
+		uint32_t ds, uint32_t interruptNumber, ErrorCode errorCode, uint32_t eip,
 		uint32_t cs, uint32_t eflags, uint32_t useresp, uint32_t ss);
 
 #endif// ISRDISPATCHER_HPP
--- src/kernel/interrupts/isrDispatcher.cpp
+++ src/kernel/interrupts/isrDispatcher.cpp
@@ -1,10 +1,23 @@
 #include <Printer.hpp>
 #include <kernel/interrupts/isrDispatcher.hpp>
+#include <kernel/interrupts/PIC.hpp>
 
 extern "C" void isrDispatcher(uint32_t gs, uint32_t fs, uint32_t es,
-		uint32_t ds, uint32_t interruptNumber, uint32_t errorCode, uint32_t eip,
+		uint32_t ds, uint32_t interruptNumber, ErrorCode errorCode, uint32_t eip,
 		uint32_t cs, uint32_t eflags, uint32_t useresp, uint32_t ss)
 {
-	out << "Interrupt number " << interruptNumber << " caught (error code " << errorCode << ").\n";
+	out << "Interrupt number " << interruptNumber << " caught (error code "
+			<< errorCode.segmentSelectorIndex << ", gdtLdt = "
+			<< errorCode.gdtLdt << ", descLoc = "
+			<< errorCode.descriptorLocation << ").\n";
+	// If the interrupt is a mapped IRQ, then send a EOI command to PIC
+	if(interruptNumber > pic::masterOffset and interruptNumber < pic::slaveOffset + 8)
+		pic::sendEndOfInterrupt(interruptNumber);
+	if(interruptNumber == 32 or interruptNumber == 33 or interruptNumber == 13)
+	{
+		// Wait before next interrupt
+		for(int i{0}; i < 10000000; ++i);
+		return;
+	}
 	while(true);
 }
