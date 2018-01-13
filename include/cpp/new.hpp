b87915a96556a57057dc72dc481e0b876b18f8eb
Théo Verhelst <tverhels@ulb.ac.be>
2018-01-13 17:36:25 +0000

    Use noexcept specifier on operator new (avoid pedantic warning)

--- include/cpp/new.hpp
+++ include/cpp/new.hpp
@@ -19,31 +19,31 @@ void operator delete[](void* address, size_t size);
 
 // In some cases, the placement new appear to be defined, but not declared,
 // in hosted environment. So we need to declare outside the condition.
-inline void* operator new(size_t size, void* address);
+inline void* operator new(size_t size, void* address) noexcept;
 
-inline void* operator new[](size_t size, void* address);
+inline void* operator new[](size_t size, void* address) noexcept;
 
-inline void operator delete(void* address, void*);
+inline void operator delete(void* address, void*) noexcept;
 
-inline void operator delete[](void* address, void*);
+inline void operator delete[](void* address, void*) noexcept;
 
 #if not __STDC_HOSTED__
 
-inline void* operator new(size_t, void* address)
+inline void* operator new(size_t, void* address) noexcept
 {
 	return address;
 }
 
-inline void* operator new[](size_t, void* address)
+inline void* operator new[](size_t, void* address) noexcept
 {
 	return address;
 }
 
-inline void operator delete(void*, void*)
+inline void operator delete(void*, void*) noexcept
 {
 }
 
-inline void operator delete[](void*, void*)
+inline void operator delete[](void*, void*) noexcept
 {
 }
 
