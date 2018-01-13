1d6cf76915174d7b5297ffd3eff4132511966f14
Théo Verhelst <tverhels@ulb.ac.be>
2018-01-13 17:51:44 +0000

    Pass the right object to the kernel

--- src/boot/kernelMain.cpp
+++ src/boot/kernelMain.cpp
@@ -9,6 +9,6 @@ extern "C" void kernelMain(const multiboot::MultibootInfo& multibootInfoAddress)
     idt::initializeIdt();
     pic::initializePic();
     const MemoryMap memoryMap{multibootInfoAddress};
-    Kernel kernel{multibootInfoAddress};
+    Kernel kernel{memoryMap};
 	kernel.run();
 }
