7c5dccfc3081cbfdfd77c6d2bfc532690b72aba7
Théo Verhelst <tverhels@ulb.ac.be>
2018-01-14 20:12:15 +0000

    Add an option to run script

--- config/run.py
+++ config/run.py
@@ -11,23 +11,28 @@ parser.add_argument(
 parser.add_argument(
     "-m", "--memory", dest="memory", type=str, default="4G",
     help="quantity of memory to simulate in the emulator, 4G by default")
+parser.add_argument(
+    "-c", "--no-continue", dest="no_continue", action="store_true",
+    help="if debug mode is used, do not automatically continue the debugger at"
+    "startup")
 
 args = parser.parse_args()
-debug = args.debug
-memory = args.memory
 
-if debug:
+if args.debug:
+    continue_arg = "-ex 'c'" if not args.no_continue else ""
+
     commands = [[
-        terminal, "-e", "qemu-system-i386 -cdrom TheOS.iso -m " + memory +
+        terminal, "-e", "qemu-system-i386 -cdrom TheOS.iso -m " + args.memory +
         " -S -s -d int -monitor stdio"
     ], [
         terminal, "-e",
-        "gdb -ex 'target remote localhost:1234' -ex 'file TheOS.bin' -ex 'c' -quiet"
+        "gdb -ex 'target remote localhost:1234' -ex 'file TheOS.bin' "
+        + continue_arg + " -quiet"
     ]]
 else:
     commands = [[
-        "qemu-system-i386", "-cdrom", "TheOS.iso", "-m", memory, "-monitor",
-        "stdio"
+        "qemu-system-i386", "-cdrom", "TheOS.iso", "-m", args.memory,
+        "-monitor", "stdio"
     ]]
 
 [process.wait() for process in
