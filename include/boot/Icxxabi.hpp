b82893efb67f2fedf8965013c59aef803a1824b4
Théo Verhelst <tverhels@ulb.ac.be>
2018-01-22 21:44:11 +0000

    I think we don't need __cxa_atexit stuff
    The function __cxa_atexit should be called whenever an object is
    constructed, and it is used to register constructors to be called at the
    exit of the kernel. But some indices led me to think that it may not
    apply to the ABI used by i386-elf-g++, although it is not clear at all.
    Anyway, what is clear is that this function is never called at startup
    of the kernel, even though some global object constructors are being
    called. So I removed all this code, it is still in the history if we
    ever need it, and on OSDev.org.

--- include/boot/Icxxabi.hpp
+++ include/boot/Icxxabi.hpp
@@ -1,25 +1,8 @@
 #ifndef ICXXABI_HPP
 #define ICXXABI_HPP
 
-#define ATEXIT_MAX_FUNCS 128
-
 extern "C"
 {
-
-typedef unsigned uarch_t;
-
-struct atexit_func_entry_t
-{
-	/// Each member is at least 4 bytes large. Such that each entry is 12bytes.
-	/// 128 * 12 = 1.5KB exact.
-	void (*destructor_func)(void *);
-	void *obj_ptr;
-	void *dso_handle;
-};
-
-int __cxa_atexit(void (*f)(void *), void *objptr, void *dso);
-
-void __cxa_finalize(void *f);
 
 /// Function called when a pure virtual function is called. It aborts with an
 /// error message.
